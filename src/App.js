65677bf3397f62a631ff724e1490024d4b1bf2d7
safabgunerr <safabguner54@gmail.com>
2025-02-28 09:53:46 +0300

    Başlık değiştirdim

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archis Academy Github Kursuna Hoşgeldiniz.
+          Archis Academy Github Kursuna Hoşgeldiniz Happy Coding.
         </p>
         <a
           className="App-link"
